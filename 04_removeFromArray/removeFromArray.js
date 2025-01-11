4ea61ceed7f5b4bf8618581409bb8618d1eeede8
mrrayoon <mrrayoonz31@gmail.com>
2025-01-12 04:51:36 +0800

    Add: finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...target_elements) {
+  const new_array = [];
+
+  array.forEach((element) => {
+    if (!target_elements.includes(element)) {
+      new_array.push(element);
+    }
+  })
+
+  return new_array;
 };
 
 // Do not edit below this line
