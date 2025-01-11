489b9f5becfbaee87cef3f9d3fb0adea49a57402
mrrayoon <mrrayoonz31@gmail.com>
2025-01-12 03:47:54 +0800

    Add: finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input_string) {
+  let result_string = "";
 
+  for (let i = input_string.length - 1; i >= 0; i--) {
+    result_string += input_string[i];
+  }
+
+  return result_string;
 };
 
 // Do not edit below this line
