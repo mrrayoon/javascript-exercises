86e3fe1c22be821447228afc451a7030b6b50a6f
mrrayoon <mrrayoonz31@gmail.com>
2025-01-12 01:51:34 +0800

    Add: finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string_argument, num) {
+  if (num < 0)
+    return "ERROR";
 
+  let result_string = "";
+
+  for (let i = 0; i < num; i++ ) {
+    result_string += string_argument;
+  }
+
+  return result_string;
 };
 
 // Do not edit below this line
