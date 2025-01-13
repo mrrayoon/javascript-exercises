2da41a77ea310972583c61c7e69ee1f75c96f0a9
mrrayoon <mrrayoonz31@gmail.com>
2025-01-13 21:48:21 +0800

    Add: finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start_number, end_number) {
+  let total_sum = 0;
 
+  // Returns 'ERROR' for negative numbers
+  if (start_number < 0 || end_number < 0) {
+    return 'ERROR';
+  }
+  // Returns 'ERROR' for non-integer parameters and non-number parameters
+  if (!Number.isInteger(start_number) || !Number.isInteger(end_number)) {
+    return 'ERROR';
+  }
+
+
+  if (start_number <= end_number) {
+    for (let i = start_number; i <= end_number; i++) {
+      total_sum += i;
+    }
+  } else {
+    for (let i = start_number; i >= end_number; i--) {
+      total_sum += i;
+    }
+  }
+
+  return total_sum;
 };
 
 // Do not edit below this line
